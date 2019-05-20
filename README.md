5622740e26d253945182c4f5c148257a4568d18b
axelvestberg <axel.vestberg@gmail.com>
2019-05-20 16:56:40 +0200

    added user auth, subscribing to new vote and link

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # hackernews-node
+
+## notes
+- In chapter 3 there is an option to add crud for links, go back and try this out after finishing the barebones tutorial.
--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+const Subscription = require('./resolvers/Subscription')
+const Vote = require('./resolvers/Vote')
 
-// 1
-
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
-
-// 2
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-  },
-  Link: {
-      id: (parent) => parent.id,
-      description: (parent) => parent.description,
-      url: (parent) => parent.url,
-  }
+ Query,
+ Mutation,
+ User,
+ Link,
+ Subscription,
+ Vote
 }
 
-// 3
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers,
+	resolvers,
+	context: request => {
+		return {
+			...request,
+			prisma,
+		}
+	}
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
